d01c400f5854eeb8e336c731b0fec5111edf906d
GustavoSFer <gustavo.sfernander@gmail.com>
2025-09-21 19:38:00 -0300

    feat: iniciando o projeto

---
+++ src/test/java/com/github/gustavosfer/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gustavosfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
