33a5c8b415d2b6e93071c766e8a7777f06e4c060
GustavoSFer <gustavo.sfernander@gmail.com>
2025-09-24 21:23:56 -0300

    feat: Criado o DTO do fornecedor para receber as informaÃcoes nos metodos

--- src/main/java/com/github/gustavosfer/services/FornecedorInterface.java
+++ src/main/java/com/github/gustavosfer/services/FornecedorInterface.java
@@ -1,14 +1,15 @@
 package com.github.gustavosfer.services;
 
+import com.github.gustavosfer.dto.FornecedorRequestDTO;
 import com.github.gustavosfer.entities.Fornecedor;
 
 import java.util.List;
 
 public interface FornecedorInterface {
 
-    public Fornecedor criarFornecedor(Fornecedor fornecedor);
+    public Fornecedor criarFornecedor(FornecedorRequestDTO fornecedor);
     public List<Fornecedor> listaFornecedores();
     public Fornecedor findByFornecedor(Long id);
-    public Fornecedor atualizarFornecedor(Long id, Fornecedor fornecedor);
+    public Fornecedor atualizarFornecedor(Long id, FornecedorRequestDTO fornecedor);
     public Fornecedor deletarFornecedor(Long id);
 }
