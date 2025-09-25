aea59a370b7fe7a7c2bb64981bc559efe1fe7c5e
GustavoSFer <gustavo.sfernander@gmail.com>
2025-09-24 21:09:57 -0300

    feat: Criado o repository de fornecedor

---
+++ src/main/java/com/github/gustavosfer/repository/FornecedorRepository.java
@@ -0,0 +1,7 @@
+package com.github.gustavosfer.repository;
+
+import com.github.gustavosfer.entities.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FornecedorRepository extends JpaRepository<Long, Fornecedor> {
+}
