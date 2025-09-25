a30554e2601f2f8c812b94453b27b082ff48f75c
GustavoSFer <gustavo.sfernander@gmail.com>
2025-09-24 21:25:14 -0300

    feat: Implementador a interface na classe service do fornecedor

--- src/main/java/com/github/gustavosfer/services/impl/FornecedorService.java
+++ src/main/java/com/github/gustavosfer/services/impl/FornecedorService.java
@@ -1,14 +1,42 @@
 package com.github.gustavosfer.services.impl;
 
+import com.github.gustavosfer.dto.FornecedorRequestDTO;
 import com.github.gustavosfer.entities.Fornecedor;
 import com.github.gustavosfer.repository.FornecedorRepository;
+import com.github.gustavosfer.services.FornecedorInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class FornecedorService {
+public class FornecedorService implements FornecedorInterface {
 
     @Autowired
     private FornecedorRepository fornecedorRepository;
 
+    @Override
+    public Fornecedor criarFornecedor(FornecedorRequestDTO fornecedor) {
+        return null;
+    }
+
+    @Override
+    public List<Fornecedor> listaFornecedores() {
+        return List.of();
+    }
+
+    @Override
+    public Fornecedor findByFornecedor(Long id) {
+        return null;
+    }
+
+    @Override
+    public Fornecedor atualizarFornecedor(Long id, FornecedorRequestDTO fornecedor) {
+        return null;
+    }
+
+    @Override
+    public Fornecedor deletarFornecedor(Long id) {
+        return null;
+    }
 }
