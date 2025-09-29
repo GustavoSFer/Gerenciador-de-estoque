c629d361050c03a22373be6f1353e4a3d7081de5
GustavoSFer <gustavo.sfernander@gmail.com>
2025-09-28 21:18:24 -0300

    feat: quantidade de caracteres para o nome do fornecedor

--- src/main/java/com/github/gustavosfer/dto/FornecedorRequestDTO.java
+++ src/main/java/com/github/gustavosfer/dto/FornecedorRequestDTO.java
@@ -2,9 +2,11 @@ package com.github.gustavosfer.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record FornecedorRequestDTO(
         @JsonProperty("nome")
+        @Size(max = 30, min = 6, message = "O nome deve conter pelo menos {min} e maximo de {max} de caracteres")
         @NotNull(message = "O nome deve ser informado.")
         String nome,
         @JsonProperty("telefone")
