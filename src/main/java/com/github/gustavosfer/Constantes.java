9ecf0b2379f54d44c8b9242360a8e465d3e496b0
GustavoSFer <gustavo.sfernander@gmail.com>
2025-10-01 21:34:02 -0300

    feat: Adicionando os logs de info

---
+++ src/main/java/com/github/gustavosfer/Constantes.java
@@ -0,0 +1,12 @@
+package com.github.gustavosfer;
+
+public class Constantes {
+
+    // FORNECEDOR
+    public static String INICIO_CADASTR0_FORNECEDOR = "Iniciando o fluxo de cadastro do fornecedor";
+    public static String FIM_CADASTR0_FORNECEDOR = "Finalizado o fluxo de cadastro do fornecedor";
+    public static String INICIO_LISTAR_FORNECEDOR = "Iniciando o fluxo de criar a lista dos fornecedores";
+    public static String FIM_LISTAR_FORNECEDOR = "Finalizado o fluxo de criar a lista dos fornecedores";
+    public static String INICIO_BUSCAR_FORNECEDOR = "Iniciando o fluxo de buscar o fornecedor pelo id";
+    public static String FIM_BUSCAR_FORNECEDOR = "Finalizado o fluxo de buscar o fornecedor pelo id";
+}
--- src/main/java/com/github/gustavosfer/controllers/FornecedorController.java
+++ src/main/java/com/github/gustavosfer/controllers/FornecedorController.java
@@ -3,41 +3,51 @@ package com.github.gustavosfer.controllers;
 import com.github.gustavosfer.dto.FornecedorRequestDTO;
 import com.github.gustavosfer.entities.Fornecedor;
 import com.github.gustavosfer.services.FornecedorInterface;
-import com.github.gustavosfer.services.impl.FornecedorService;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static com.github.gustavosfer.Constantes.*;
+
 @RestController
 @RequestMapping("/fornecedores")
 public class FornecedorController {
+
+    private final Logger logger = LoggerFactory.getLogger(FornecedorController.class);
 
     @Autowired
     private FornecedorInterface fornecedorService;
 
     @PostMapping
     public ResponseEntity<Fornecedor> cadastrarFornecedor(@Valid @RequestBody FornecedorRequestDTO fornecedorRequestDTO) {
+        logger.info("[Controller] - " + INICIO_CADASTR0_FORNECEDOR);
         Fornecedor fornecedor = fornecedorService.criarFornecedor(fornecedorRequestDTO);
 
+        logger.info("[Controller] - " + FIM_CADASTR0_FORNECEDOR);
         return ResponseEntity.ok().body(fornecedor);
     }
 
 
     @GetMapping
     public ResponseEntity<List<Fornecedor>> listarFornecedor() {
+        logger.info("[Controller] - " + INICIO_LISTAR_FORNECEDOR);
         List<Fornecedor> fornecedores = fornecedorService.listaFornecedores();
 
+        logger.info("[Controller] - " + FIM_LISTAR_FORNECEDOR);
         return ResponseEntity.ok().body(fornecedores);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Fornecedor> buscarFornecedorById(@PathVariable Long id) {
+        logger.info("[Controller] - " + INICIO_BUSCAR_FORNECEDOR);
         Fornecedor fornecedor = fornecedorService.findByFornecedor(id);
 
+        logger.info("[Controller] - " + FIM_BUSCAR_FORNECEDOR);
         return ResponseEntity.ok().body(fornecedor);
     }
 }
