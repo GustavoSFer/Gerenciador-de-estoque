dee0cbbd777a76b5a572b59127e9c1892a763172
GustavoSFer <gustavo.sfernander@gmail.com>
2025-10-15 21:44:32 -0300

    feat: Criado a service e implementando a interface

---
+++ src/main/java/com/github/gustavosfer/services/impl/CategoriaService.java
@@ -0,0 +1,33 @@
+package com.github.gustavosfer.services.impl;
+
+import com.github.gustavosfer.entities.Categoria;
+import com.github.gustavosfer.services.CategoriaInterface;
+
+import java.util.List;
+
+public class CategoriaService implements CategoriaInterface {
+    @Override
+    public Categoria criarCategoria(Categoria categoria) {
+        return null;
+    }
+
+    @Override
+    public List<Categoria> listarCategoria() {
+        return List.of();
+    }
+
+    @Override
+    public Categoria buscarCategoria(Long id) {
+        return null;
+    }
+
+    @Override
+    public Categoria atualizarCategoria(Categoria categoria) {
+        return null;
+    }
+
+    @Override
+    public void deletarCategoria(Long id) {
+
+    }
+}
