830f9d25bc8930166651e276075f253791e5abb1
GustavoSFer <gustavo.sfernander@gmail.com>
2025-10-15 21:47:02 -0300

    feat: Criado o repository de categoria

--- src/main/java/com/github/gustavosfer/services/impl/CategoriaService.java
+++ src/main/java/com/github/gustavosfer/services/impl/CategoriaService.java
@@ -1,11 +1,17 @@
 package com.github.gustavosfer.services.impl;
 
 import com.github.gustavosfer.entities.Categoria;
+import com.github.gustavosfer.repository.CategoriaRepository;
 import com.github.gustavosfer.services.CategoriaInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 public class CategoriaService implements CategoriaInterface {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
     @Override
     public Categoria criarCategoria(Categoria categoria) {
         return null;
