219cdf626951298b00e0e080a71c4cb45343eed0
GustavoSFer <gustavo.sfernander@gmail.com>
2025-10-22 21:16:36 -0300

    feat: removido imports nao usados

--- src/main/java/com/github/gustavosfer/dto/FornecedorRequestDTO.java
+++ src/main/java/com/github/gustavosfer/dto/FornecedorRequestDTO.java
@@ -3,7 +3,6 @@ package com.github.gustavosfer.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import org.springframework.hateoas.RepresentationModel;
 
 public record FornecedorRequestDTO (
         @JsonProperty("nome")
