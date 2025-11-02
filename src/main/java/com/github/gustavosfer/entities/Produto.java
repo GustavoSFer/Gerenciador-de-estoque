a2ca903559e4b218e7162e81256a6561cfb4ece0
GustavoSFer <gustavo.sfernander@gmail.com>
2025-11-02 16:36:45 -0300

    fix: contrutor da entidade

--- src/main/java/com/github/gustavosfer/entities/Produto.java
+++ src/main/java/com/github/gustavosfer/entities/Produto.java
@@ -1,14 +1,18 @@
 package com.github.gustavosfer.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Produto {
 
